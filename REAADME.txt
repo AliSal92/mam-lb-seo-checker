0e56d884bc22b1d1229b63d0e2ae4b97e9006cdf
Ali Sal <ali.alsalti1992@gmail.com>
2021-07-02 14:01:48 +0700

    Initial commit

---
+++ REAADME.txt
@@ -0,0 +1,49 @@
+# MAM Linkbuilding SEO CHECKER ADD ON
+
+=== MAM Linkbuilding SEO CHECKER ADD ON ===
+
+Contributors: alisal
+
+Tags: Move Ahead Media, Link building system, SEO CHECKER ADD ON
+
+Donate link: paypal.me/Peace71
+
+Requires at least: 5.4.2
+
+Tested up to: 5.4.2
+
+Requires PHP: 5.6
+
+Stable tag: 1.0
+
+License: GPLv2
+
+License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+Used as an add-on for mam/linkbuilding plugin to auto update the resources metrics using SEO CHECKER API
+
+== Description ==
+
+Used as an add-on for mam/linkbuilding plugin to auto update the resources metrics using SEO CHECKER API
+== Installation ==
+
+* Upload ”mam-lb-seo-checker” to the ”/wp-content/plugins/” directory.
+* Activate the plugin through the ”Plugins” menu in WordPress.
+* Add the “MAM Linkbuilding SEO CHECKER ADD ON” to the page where you want to show.
+
+== Frequently Asked Questions ==
+
+No FAQs at the moment, feel free to discuss issues in with support.
+
+== Screenshots ==
+
+1. Front-end screenshot.
+
+2. Back-end screenshot.
+
+== Changelog ==
+
+= 1.0 =
+
+* first version!
+
---
+++ mam-lbl-seo-checker.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: MAM Linkbuilding SEO CHECKER ADD ON
+ * Plugin URI: https://github.com/AliSal92/mam-lb-seo-checker
+ * Description: Used as an add-on for mam/linkbuilding plugin to auto update the resources metrics using SEO CHECKER API
+ * Version: 1.0
+ * Author: AliSal
+ * Text Domain: mam-lb-seo-checker
+ * Author URI: https://github.com/AliSal92/
+ * MAM Linkbuilding SEO CHECKER ADD ON System is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * MAM Linkbuilding SEO CHECKER ADD ON System is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MAM Linkbuilding SEO CHECKER ADD ON System. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace MAM;
+
+use MAM\Plugin\Init;
+
+
+/**
+ * Prevent direct access
+ */
+defined('ABSPATH') or die('</3');
+
+
+/**
+ * Require once the Composer Autoload
+ */
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+    require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+/**
+ * Initialize and run all the core classes of the plugin
+ */
+if ( class_exists( 'MAM\Plugin\Init' ) ) {
+    Init::register_services();
+}
